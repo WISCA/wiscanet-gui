2ab265bcecdd39a713ee5ee6f4b7439c252fe357
Jacob Holtom <jacob@holtom.me>
2020-09-12 10:35:43 -0700

    Working on our way to a usable database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,94 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_migrations;
+#[macro_use] extern crate log;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate rocket_contrib;
+
+mod edgenode;
+
+use rocket::Rocket;
+use rocket::fairing::AdHoc;
+use rocket::request::{Form, FlashMessage};
+use rocket::response::{Flash, Redirect};
+use rocket_contrib::{templates::Template, serve::StaticFiles};
+use diesel::SqliteConnection;
+
+use edgenode::{Edgenode, Node};
+
+// This macro from `diesel_migrations` defines an `embedded_migrations` module
+// containing a function named `run`. This allows the example to be run and
+// tested without any outside setup of the database.
+embed_migrations!();
+
+#[database("sqlite_database")]
+pub struct DbConn(SqliteConnection);
+
+#[derive(Debug, Serialize)]
+struct Context<'a, 'b>{ msg: Option<(&'a str, &'b str)>, edgenodes: Vec<Edgenode> }
+
+impl<'a, 'b> Context<'a, 'b> {
+    pub fn err(conn: &DbConn, msg: &'a str) -> Context<'static, 'a> {
+        Context{msg: Some(("error", msg)), edgenodes: Edgenode::all(conn)}
+    }
+
+    pub fn raw(conn: &DbConn, msg: Option<(&'a str, &'b str)>) -> Context<'a, 'b> {
+        Context{msg: msg, edgenodes: Edgenode::all(conn)}
+    }
+}
+
+#[post("/", data = "<node_form>")]
+fn new(node_form: Form<Node>, conn: DbConn) -> Flash<Redirect> {
+    let node = node_form.into_inner();
+    if node.description.is_empty() {
+        Flash::error(Redirect::to("/"), "Description cannot be empty.")
+    } else if Edgenode::insert(node, &conn) {
+        Flash::success(Redirect::to("/"), "Node successfully added.")
+    } else {
+        Flash::error(Redirect::to("/"), "Whoops! The server failed.")
+    }
+}
+
+#[delete("/<id>")]
+fn delete(id: i32, conn: DbConn) -> Result<Flash<Redirect>, Template> {
+    if Edgenode::delete_with_id(id, &conn) {
+        Ok(Flash::success(Redirect::to("/"), "Node was deleted."))
+    } else {
+        Err(Template::render("index", &Context::err(&conn, "Couldn't delete edgenode.")))
+    }
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index(msg: Option<FlashMessage>, conn: DbConn) -> Template {
+    Template::render("index", &match msg {
+        Some(ref msg) => Context::raw(&conn, Some((msg.name(), msg.msg()))),
+        None => Context::raw(&conn, None),
+    })
+}
+
+fn run_db_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
+    let conn = DbConn::get_one(&rocket).expect("database connection");
+    match embedded_migrations::run(&*conn) {
+        Ok(()) => Ok(rocket),
+        Err(e) => {
+            error!("Failed to run database migrations: {:?}", e);
+            Err(rocket)
+        }
+    }
+}
+
+fn rocket() -> Rocket {
+    rocket::ignite()
+        .attach(DbConn::fairing())
+        .attach(AdHoc::on_attach("Database Migrations", run_db_migrations))
+        .mount("/", StaticFiles::from("static/"))
+        .mount("/", routes![index])
+        .mount("/node", routes![new, toggle, delete])
+        .attach(Template::fairing())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
